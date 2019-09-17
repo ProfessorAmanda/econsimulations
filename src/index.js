4027f3bb3540df2747962dd525180d6a8e2e91b0
wgg <wgg2@member.fsf.org>
2019-09-16 21:59:56 -0400

    Fix typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 import { unregister } from './registerServiceWorker';
-unregister ();
+unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
