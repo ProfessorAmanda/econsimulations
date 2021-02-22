1fad3961d10b12653d5a0430ad3fa2203b6aa055
Gregg <agregg@Midd-19529.lan>
2021-02-22 11:54:39 -0500

    Updates

---
+++ precache-manifest.592f88d54eb8b580a14f521b664ea66d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adca855d267b656dce109f9271cd704",
+    "url": "/econ-sims/index.html"
+  },
+  {
+    "revision": "073959d8e4deb9c12220",
+    "url": "/econ-sims/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "52486bbb8c05c4313872",
+    "url": "/econ-sims/static/css/main.952a9592.chunk.css"
+  },
+  {
+    "revision": "073959d8e4deb9c12220",
+    "url": "/econ-sims/static/js/2.d542e29a.chunk.js"
+  },
+  {
+    "revision": "52486bbb8c05c4313872",
+    "url": "/econ-sims/static/js/main.6392ba33.chunk.js"
+  },
+  {
+    "revision": "902cc3bba8371ad1db34",
+    "url": "/econ-sims/static/js/runtime~main.4c4c7ca3.js"
+  },
+  {
+    "revision": "00a24e293bdc74213c3292256c02dc3d",
+    "url": "/econ-sims/static/media/ECONSIMS.00a24e29.png"
+  },
+  {
+    "revision": "5be34658588c2f19e72ae77a0365d6dc",
+    "url": "/econ-sims/static/media/Intro.5be34658.gif"
+  },
+  {
+    "revision": "092239b8d258606bd1fdd479408de7df",
+    "url": "/econ-sims/static/media/bg.092239b8.png"
+  },
+  {
+    "revision": "97a1b4bdc20050bc60d21ec50aa23a73",
+    "url": "/econ-sims/static/media/clt.97a1b4bd.png"
+  },
+  {
+    "revision": "21417d9f8d5f51d41cf938c60a1a45d5",
+    "url": "/econ-sims/static/media/jd.21417d9f.jpg"
+  },
+  {
+    "revision": "9ccd6e4fed4e4e6a8936c75a014109d7",
+    "url": "/econ-sims/static/media/lln.9ccd6e4f.png"
+  },
+  {
+    "revision": "b50024315168cf03dc30abaae9eeabb6",
+    "url": "/econ-sims/static/media/ovs.b5002431.png"
+  }
+]);
