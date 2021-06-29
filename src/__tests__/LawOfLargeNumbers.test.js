1c6187644892a86cf76eafeed932d964a70534c4
Ethan Saxenian <ethansaxenian@gmail.com>
2021-06-29 11:11:53 -0400

    add lln tests

--- src/__tests__/LawOfLargeNumbers.test.js
+++ src/__tests__/LawOfLargeNumbers.test.js
@@ -1,9 +1,23 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import LawOfLargeNumbers from "../components/LawOfLargeNumbers/LawOfLargeNumbers.js";
-import { dataFromDistribution } from "../lib/stats-utils.js";
+import LLNSimulation from "../components/LawOfLargeNumbers/LLNSimulation.js";
+import { dataFromDistribution, populationMean } from "../lib/stats-utils.js";
+import { testPopulation } from "../lib/test-utils.js";
+
+jest.mock("../lib/stats-utils.js", () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual("../lib/stats-utils.js");
+
+  return {
+    ...originalModule,
+    // only mock dataFromDistribution function
+    dataFromDistribution: jest.fn(),
+  };
+});
 
 describe("LawOfLargeNumbers integration tests", () => {
   beforeEach(() => {
+    dataFromDistribution.mockReturnValue(testPopulation);
     render(<LawOfLargeNumbers/>);
   });
 
@@ -17,8 +31,21 @@ describe("LawOfLargeNumbers integration tests", () => {
   });
 });
 
-// describe("LLNSimulation tests", () => {
-//   beforeEach(() => {
-//     dataFromDistribution.mockReturnValue()
-//   });
-// });
+describe("LLNSimulation tests", () => {
+
+  beforeEach(() => {
+    dataFromDistribution.mockReturnValue(testPopulation);
+    render(<LLNSimulation popShape={"Normal"} sampleSize={100}/>);
+  });
+
+  test("correct population mean is displayed", () => {
+    expect(screen.queryByText(`POPULATION MEAN: ${populationMean(testPopulation)}`, { exact: false })).toBeInTheDocument();
+  });
+
+  test("selecting sample shows sample mean alert and simulation", () => {
+    fireEvent.change(screen.getByRole("spinbutton"), { target: { value: 10 }});
+    fireEvent.click(screen.getByRole("button", { name: "Sample" }));
+    expect(screen.getByText("Difference of Means:", { exact: false })).toBeInTheDocument();
+    expect(screen.getByText("POPULATION VS SAMPLE MEANS", { exact: false })).toBeInTheDocument();
+  });
+});
