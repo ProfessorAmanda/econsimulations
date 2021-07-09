3e96cb4871bbc3e23d858e50f00b4446da5f569a
David G <dgikoshvili@middlebury.edu>
2021-07-09 14:37:13 -0400

    Set up buttons and new file

--- src/components/SampleDistributionOLSEstimators/InterceptDistributionPlot.js
+++ src/components/SampleDistributionOLSEstimators/InterceptDistributionPlot.js
@@ -3,12 +3,15 @@ import { max, min } from 'mathjs';
 import PropTypes from 'prop-types';
 import { olsSampleType } from '../../lib/types.js';
 import { getCounts } from '../../lib/stats-utils.js';
+import { Form } from 'react-bootstrap';
+import { useState } from 'react';
+
 
 export default function InterceptDistributionPlot({ samples }) {
   const plotData = getCounts(samples.map(({ intercept }) => intercept));
+  const [standardized, setStandardized] = useState(false);
 
-  return (
-    <DotPlot
+  let plot = <DotPlot
       series={[{ name: 'intercepts', data: plotData }]}
       title="Distribution of Sample Intercepts"
       xMin={min(20, ...plotData.map(({ x }) => x))}
@@ -16,8 +19,18 @@ export default function InterceptDistributionPlot({ samples }) {
       yMax={max(4, ...plotData.map(({ y }) => y))}
       xLabel="Intercept"
     />
-  )
+if (standardized) {
+  plot = <> </>
 }
+  return  <>
+    {plot}
+    <Form.Check
+    inline
+    type="checkbox"
+    label="Convert to Standard Normal"
+     onClick={() => setStandardized(!standardized)}/>  
+     </>
+  }
 
 InterceptDistributionPlot.propTypes = {
   samples: PropTypes.arrayOf(olsSampleType).isRequired
--- src/components/SampleDistributionOLSEstimators/SlopeDistributionPlot.js
+++ src/components/SampleDistributionOLSEstimators/SlopeDistributionPlot.js
@@ -3,21 +3,33 @@ import { max, min } from 'mathjs';
 import PropTypes from 'prop-types';
 import { olsSampleType } from '../../lib/types.js';
 import { getCounts } from '../../lib/stats-utils.js';
+import { Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 export default function SlopeDistributionPlot({ samples }) {
   const plotData = getCounts(samples.map(({ slope }) => slope));
+  const [standardized, setStandardized] = useState(false);
 
-  return (
-    <DotPlot
-      series={[{ name: 'slopes', data: plotData }]}
-      title="Distribution of Sample Slopes"
-      xMin={min(-5, ...plotData.map(({ x }) => x))}
-      xMax={max(5, ...plotData.map(({ x }) => x))}
-      yMax={max(4, ...plotData.map(({ y }) => y))}
-      xLabel="Slope"
-    />
-  )
+  let plot = <DotPlot
+  series={[{ name: 'slopes', data: plotData }]}
+  title="Distribution of Sample Slopes"
+  xMin={min(-5, ...plotData.map(({ x }) => x))}
+  xMax={max(5, ...plotData.map(({ x }) => x))}
+  yMax={max(4, ...plotData.map(({ y }) => y))}
+  xLabel="Slope"
+/> 
+if (standardized) {
+  plot = <> </>
 }
+  return  <>
+    {plot}
+    <Form.Check
+    inline
+    type="checkbox"
+    label="Convert to Standard Normal"
+     onClick={() => setStandardized(!standardized)}/>  
+     </>
+  }
 
 SlopeDistributionPlot.propTypes = {
   samples: PropTypes.arrayOf(olsSampleType).isRequired
