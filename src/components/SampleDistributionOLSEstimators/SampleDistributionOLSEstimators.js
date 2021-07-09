cc2c34b64284c4e754a4eb2b75ff4ba1251da294
Ethan Saxenian <ethansaxenian@gmail.com>
2021-07-09 14:08:33 -0400

    fix lint

--- src/components/SampleDistributionOLSEstimators/SampleDistributionOLSEstimators.js
+++ src/components/SampleDistributionOLSEstimators/SampleDistributionOLSEstimators.js
@@ -1,10 +1,10 @@
 import { Alert } from 'react-bootstrap';
 import SDOLSESimulation from './SDOLSESimulation.js';
-import SelectorButtonGroup from "../SelectorButtonGroup.js";
-import { useState } from "react";
+import SelectorButtonGroup from '../SelectorButtonGroup.js';
+import { useState } from 'react';
 
 export default function SampleDistributionOLSEstimators() {
-  const [dataShape, setDataShape] = useState("Scatter");
+  const [dataShape, setDataShape] = useState('Scatter');
 
   return (
     <div className="module-container">
@@ -12,7 +12,7 @@ export default function SampleDistributionOLSEstimators() {
         Sample Distribution of OLS Estimators
       </Alert>
       <p>Select a population shape:</p>
-      <SelectorButtonGroup options={["Scatter", "Binary"]} select={setDataShape} selected={dataShape}/>
+      <SelectorButtonGroup options={['Scatter', 'Binary']} select={setDataShape} selected={dataShape}/>
       <br/>
       <SDOLSESimulation/>
     </div>
