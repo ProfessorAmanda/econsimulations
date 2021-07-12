531b139dc45bd726cf5696269bd817cd377d0a2d
Ethan Saxenian <ethansaxenian@gmail.com>
2021-07-12 10:57:00 -0400

    start with no selection

--- src/components/SampleDistributionOLSEstimators/SampleDistributionOLSEstimators.js
+++ src/components/SampleDistributionOLSEstimators/SampleDistributionOLSEstimators.js
@@ -4,17 +4,19 @@ import SelectorButtonGroup from '../SelectorButtonGroup.js';
 import { useState } from 'react';
 
 export default function SampleDistributionOLSEstimators() {
-  const [populationShape, setPopulationShape] = useState('Scatter');
+  const [populationShape, setPopulationShape] = useState('');
 
   return (
     <div className="module-container">
       <Alert style={{ width: '50%', margin: 'auto' }} variant="primary">
         Sample Distribution of OLS Estimators
       </Alert>
+      <br/>
       <p>Select a population shape:</p>
       <SelectorButtonGroup options={['Scatter', 'Binary']} select={setPopulationShape} selected={populationShape}/>
       <br/>
-      <SDOLSESimulation populationShape={populationShape}/>
+      <br/>
+      {populationShape && <SDOLSESimulation populationShape={populationShape}/>}
     </div>
   );
 }
