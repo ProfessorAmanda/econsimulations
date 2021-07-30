8ccad4e7fb4d90a7d85f6d1fb00337b9edab9ff5
Ethan Saxenian <ethansaxenian@gmail.com>
2021-07-30 15:25:33 -0400

    fix tests

--- src/components/SimBarOption.js
+++ src/components/SimBarOption.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function SimBarOption({ section, setSection }) {
   return (
-    <Card border="primary" className="menu-item" onClick={() => setSection(section.name)}>
+    <Card border="primary" className="menu-item" data-testid={`module-${section.name}`} onClick={() => setSection(section.name)}>
       <Card.Header>{section.name}</Card.Header>
       <Card.Body>
         <Card.Text className="menu-item-text">{section.description}</Card.Text>
