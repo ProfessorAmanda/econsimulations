35560ec00c5f2d1009670a0b9763354c961eca7a
Ethan Saxenian <ethansaxenian@gmail.com>
2021-07-30 15:34:54 -0400

    switch block math to inline math

--- src/components/OmittedVariableBias/OmittedVariableBiasContainer.js
+++ src/components/OmittedVariableBias/OmittedVariableBiasContainer.js
@@ -1,6 +1,6 @@
 import { Alert, Row } from 'react-bootstrap';
 import OmittedVariableBias from './OmittedVariableBias';
-import { BlockMath, InlineMath } from 'react-katex';
+import { InlineMath } from 'react-katex';
 
 export default function OmittedVariableBiasContainer() {
   return (
@@ -9,8 +9,7 @@ export default function OmittedVariableBiasContainer() {
         Omitted Variable Bias
       </Alert>
       <Alert className="sim-description" variant="primary">
-        Omitted Variable Bias (OVB) is the bias in a regression estimator that arises when there is a variable (<InlineMath math="V"/>) which is not included in the regression that is correlated with the regressor (<InlineMath math="X"/>) and is a determinant of the outcome (<InlineMath math="Y"/>). In the regression model <InlineMath math="Y_i = \beta_0 + \beta_1 X_i + \delta V_i + \epsilon_i"/> where <InlineMath math="V"/> is omitted from the regression estimation, the OVB is described as the final term in the following expression:
-        <BlockMath math="\beta_1 \xrightarrow{p} \hat{\beta}_1 + \frac{\delta Cov(X,V)}{Var(X)}"/>
+        Omitted Variable Bias (OVB) is the bias in a regression estimator that arises when there is a variable (<InlineMath math="V"/>) which is not included in the regression that is correlated with the regressor (<InlineMath math="X"/>) and is a determinant of the outcome (<InlineMath math="Y"/>). In the regression model <InlineMath math="Y_i = \beta_0 + \beta_1 X_i + \delta V_i + \epsilon_i"/> where <InlineMath math="V"/> is omitted from the regression estimation, the OVB is described as the final term in the following expression: <InlineMath math="\beta_1 \xrightarrow{p} \hat{\beta}_1 + \frac{\delta Cov(X,V)}{Var(X)}"/>
       </Alert>
       <br/>
       <Row>
