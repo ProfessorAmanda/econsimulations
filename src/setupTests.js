202b86120ead4acdca0e78c7b3f3b3a21c2ef64f
Ethan Saxenian <ethansaxenian@gmail.com>
2021-08-02 15:34:30 -0400

    fix lint

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,5 @@ import '@testing-library/jest-dom';
 
 // need to mock these two functions as Plotly runs in the browser, while tests run in Node
 // see https://github.com/plotly/react-plotly.js/issues/115
-window.HTMLCanvasElement.prototype.getContext = () => {};
-window.URL.createObjectURL = () => {};
+window.HTMLCanvasElement.prototype.getContext = () => {};  // eslint-disable-line
+window.URL.createObjectURL = () => {};  // eslint-disable-line
