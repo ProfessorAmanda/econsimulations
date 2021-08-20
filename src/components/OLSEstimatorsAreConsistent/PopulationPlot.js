c0115225f53d4c6425d996c646c4d3a89d717949
Ethan Saxenian <ethansaxenian@gmail.com>
2021-08-20 15:22:42 -0400

    show state of violation with switch

--- src/components/OLSEstimatorsAreConsistent/PopulationPlot.js
+++ src/components/OLSEstimatorsAreConsistent/PopulationPlot.js
@@ -56,7 +56,7 @@ export default function PopulationPlot({ data, selected, assumption, showViolati
               checked={showViolation}
               inline
               className="form-switch"
-              label="Toggle Violation"
+              label={`Toggle Violation ${showViolation ? 'OFF' : 'ON'}`}
               onChange={() => setShowViolation(!showViolation)}
             />
             )}
