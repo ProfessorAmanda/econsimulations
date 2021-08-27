113d53ffa5825db3a15ca2ca21fa1af83d3afd67
Ethan Saxenian <ethansaxenian@gmail.com>
2021-08-27 10:33:47 -0400

    convert simulate samples

--- src/components/SimulateSamples.js
+++ src/components/SimulateSamples.js
@@ -1,46 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
 import { Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { dataObjectArrayType, optionalLaTeXType } from '../lib/types.js';
+import { Chart, HighchartsChart, HighchartsProvider, Legend, LineSeries, Title, XAxis, YAxis } from 'react-jsx-highcharts';
 
 export default function SimulateSamples({ title, mathTitle, popArray, sampleSeriesName, popValSeriesName, yLabel, sampleFn, yFn }) {
   const [sampled, setSampled] = useState([]);
   const [meanLine, setMeanLine] = useState([]);
   const [start, setStart] = useState(false);
-  const [chart, setChart] = useState({
-    chart: {
-      type: 'line',
-      animation: false
-    },
-    plotOptions: {
-      series: {
-        states: {
-          hover: {
-            enabled: false
-          }
-        }
-      }
-    },
-    title: {
-      text: title || ''
-    },
-    xAxis: {
-      title: {
-        text: 'Sample Size'
-      },
-      min: 0
-    },
-    yAxis: {
-      title: {
-        text: yLabel
-      }
-    },
-    tooltip: {
-      enabled: false
-    }
-  });
 
   const timer = useRef();
 
@@ -50,37 +18,6 @@ export default function SimulateSamples({ title, mathTitle, popArray, sampleSeri
     setStart(false);
     clearInterval(timer.current);
   }, [mathTitle]);
-
-  useEffect(() => {
-    const newChart = {
-      series: [
-        {
-          name: popValSeriesName,
-          data: meanLine,
-          label: {
-            enabled: false
-          },
-          marker: {
-            enabled: false
-          },
-          color: 'red'
-        },
-        {
-          name: sampleSeriesName,
-          data: sampled,
-          label: {
-            enabled: false
-          },
-          marker: {
-            enabled: false
-          },
-          color: 'black'
-        }
-      ]
-    }
-
-    setChart(newChart);
-  }, [sampled, meanLine, popValSeriesName, sampleSeriesName]);
 
   useEffect(() => {
     return () => clearInterval(timer.current);
@@ -130,7 +67,33 @@ export default function SimulateSamples({ title, mathTitle, popArray, sampleSeri
   return (
     <Card body>
       {mathTitle && mathTitle}
-      <HighchartsReact highcharts={Highcharts} options={chart}/>
+      <HighchartsProvider Highcharts={Highcharts}>
+        <HighchartsChart>
+          <Chart animation={false}/>
+          <Title>{title || ''}</Title>
+          <XAxis min={0}>
+            <XAxis.Title>Sample Size</XAxis.Title>
+          </XAxis>
+          <YAxis>
+            <YAxis.Title>{yLabel}</YAxis.Title>
+            <LineSeries
+              name={popValSeriesName}
+              data={meanLine}
+              color="red"
+              marker={false}
+              states={{hover: false}}
+            />
+            <LineSeries
+              name={sampleSeriesName}
+              data={sampled}
+              marker={false}
+              color="black"
+              states={{hover: false}}
+            />
+          </YAxis>
+          <Legend/>
+        </HighchartsChart>
+      </HighchartsProvider>
       <Button
         variant={`outline-${start ? 'danger' : 'success'}`}
         onClick={() => toggleSim()}
