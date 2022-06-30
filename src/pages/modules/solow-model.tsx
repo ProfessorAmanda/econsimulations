590fe086d436fc52fc0838c994fcf1e4f4eadf43
Wayne Wang <sw@middlebury.edu>
2022-06-30 18:10:39 -0400

    adapted solow model for new routing structure

---
+++ src/pages/modules/solow-model.tsx
@@ -0,0 +1,10 @@
+import SolowModel from 'src/modules/SolowModel/SolowModel';
+import ModuleWrapper from 'src/components/ModuleWrapper';
+
+export default function SolowModelPage() {
+  return (
+    <ModuleWrapper>
+      <SolowModel />
+    </ModuleWrapper>
+  )
+}
