ebec7073bcc4c308fa777fe090311f34e67f2155
Wayne Wang <sw@middlebury.edu>
2022-08-02 17:48:40 -0400

    optimized front page for smaller screens, rewrote animation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,27 @@ import { Button } from 'react-bootstrap';
 
 export default function Footer() {
   return (
-    <p style={{ paddingBottom: '3rem' }}>
+    <div style={{ paddingBottom: '3rem', display: 'flex', justifyContent: 'center' }}>
       <Button
         size="sm"
         variant="outline-primary"
         href="https://amandagreggeconomics.com/statistics-simulations-project/"
         target="_blank"
         rel="noopener noreferrer"
+        style={{ width: '12rem' }}
       >
         View teaching materials
       </Button>
-      {' '}
       <Button
         size="sm"
         variant="outline-success"
         href="https://github.com/ProfessorAmanda/econsimulations"
         target="_blank"
         rel="noopener noreferrer"
+        style={{ marginLeft: '0.5rem', width: '12rem' }}
       >
         View code on GitHub
       </Button>
-    </p>
+    </div>
   )
 }
--- src/components/SimulationMenu.tsx
+++ src/components/SimulationMenu.tsx
@@ -1,33 +1,75 @@
 import { useEffect, useState } from 'react';
 import StartHere from './StartHere';
 import Footer from './Footer';
+import { animated, useSpring } from '@react-spring/web';
+import useWindowDimensions from 'src/lib/useWindowDimensions';
 
 
+export default function SimulationMenuNew() {
+  const [gif, setGif] = useState('');
+  const [animationDone, setAnimationDone] = useState(false);
 
-export default function SimulationMenu() {
-  const [logo, setLogo] = useState(true);
+  const { isMobile } = useWindowDimensions();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLogo(false);
-    }, 3500);
-
-    return () => clearTimeout(timer);
+    // To make sure gif replays on every reload
+    setGif('/images/Intro.gif');
   }, []);
+
+  const imgStyles = useSpring({
+    from: { y: '0rem' },
+    to: { y: '-10rem' },
+    config: {
+      mass: 1,
+      tension: 300,
+      friction: 100
+    },
+    delay: 3300, // The duration for the gif to finish playing
+    onStart: () => {
+      setTimeout(() => {
+        setAnimationDone(true);
+      }, 600);
+    }
+  });
+
+  const textStyles = useSpring({
+    config: {
+      mass: 1,
+      tension: 200,
+      friction: 60
+    },
+    opacity: animationDone ? 1 : 0,
+    y: animationDone ? 0 : -100,
+  });
+
 
   return (
     <div>
-      <div className="intro-animation" />
-      {!logo && (
-        <>
+      <animated.div style={{
+        ...imgStyles,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        backgroundImage: `url('${gif}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '100%',
+        zIndex: -1,
+      }} />
+      <animated.div style={{
+        ...textStyles,
+        marginLeft: isMobile ? '10%' : '25%',
+        marginRight: isMobile ? '10%' : '25%',
+      }}>
+        <div style={{ marginTop: '50vh' }}>
           <StartHere />
-          <br />
-          <br />
-          <br />
-          <br />
+        </div>
+        <div style={{ marginTop: '5rem' }}>
           <Footer />
-        </>
-      )}
+        </div>
+      </animated.div>
     </div>
   );
 }
